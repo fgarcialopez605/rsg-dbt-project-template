0f74463d342ff2aca54362fb990aaf1cac36969b
Francisco Garcia Lopez <francisco.gl@globant.com>
2024-12-27 13:56:19 +0100

    Casting VARIANT fields

--- models/staging/ga4/stg_ga4__event_historical_data.sql
+++ models/staging/ga4/stg_ga4__event_historical_data.sql
@@ -14,6 +14,16 @@ transformed as (
        if column.name not in ['event_date', 'event_timestamp', 'user_first_touch_timestamp'] and column.data_type != 'VARIANT' %}
         "{{ column.name }}"{% if not loop.last %}, {% endif %}
     {% endfor %},
+    CAST("collected_traffic_source" AS MAP(VARCHAR, VARCHAR)) AS "collected_traffic_source",
+    TRY_CAST("device_mobile_brand_name"::STRING AS STRING) AS "device_mobile_brand_name",
+    TRY_CAST("device_mobile_marketing_name"::STRING AS STRING) AS "device_mobile_marketing_name",
+    TRY_CAST("device_mobile_model_name"::STRING AS STRING) AS "device_mobile_model_name",
+    TRY_CAST("device_mobile_os_hardware_model"::STRING AS STRING) AS "device_mobile_os_hardware_model",
+    TRY_CAST("device_web_info_browser_version"::STRING AS STRING) AS "device_web_info_browser_version",
+    CAST("items"::VARIANT AS ARRAY(NUMBER)) AS "items",
+    CAST("session_traffic_source_last_click" AS MAP(VARCHAR, MAP(VARCHAR, VARCHAR))) AS "session_traffic_source_last_click",
+    TRY_CAST("user_ltv_currency"::STRING AS STRING) AS "user_ltv_currency",
+    CAST("user_properties"::VARIANT AS ARRAY(NUMBER)) AS "user_properties",
     TO_DATE("event_date", 'YYYYMMDD') AS "event_date",
     TO_TIMESTAMP("event_timestamp" / 1000000) AS "event_timestamp",
     TO_TIMESTAMP("user_first_touch_timestamp" / 1000000) AS "user_first_touch_timestamp"
--- models/staging/ga4/stg_ga4__pseudonymous_users_historical_data.sql
+++ models/staging/ga4/stg_ga4__pseudonymous_users_historical_data.sql
@@ -11,10 +11,14 @@ with source as (
 transformed as (
     SELECT
     {% for column in adapter.get_columns_in_relation(source('GA4_LANDING', 'PSEUDONYMOUS_USERS_HISTORICAL_DATA')) 
-       if column.name not in ['occurrence_date', 'last_updated_date', 'last_active_timestamp_micros', 'user_first_touch_timestamp_micros', 'audience_start', 'audience_expiry'] 
+       if column.name not in ['occurrence_date', 'last_updated_date', 'last_active_timestamp_micros', 'user_first_touch_timestamp_micros', 
+            'audience_start', 'audience_expiry'] 
         and column.data_type != 'VARIANT' %}
             "{{ column.name }}"{% if not loop.last %}, {% endif %}
     {% endfor %},
+    TRY_CAST("mobile_model_name"::STRING AS STRING) AS "mobile_model_name",
+    TRY_CAST("unified_screen_name"::STRING AS STRING) AS "unified_screen_name",
+    CAST("user_properties"::VARIANT AS ARRAY(NUMBER)) AS "user_properties",
     TO_TIMESTAMP("last_active_timestamp_micros" / 1000000) AS "last_active_timestamp_micros",
     TO_TIMESTAMP("user_first_touch_timestamp_micros" / 1000000) AS "user_first_touch_timestamp_micros",
     TO_TIMESTAMP("audience_start" / 1000000) AS "audience_start",
